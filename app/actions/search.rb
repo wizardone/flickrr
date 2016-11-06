c69cddd07a0b113b01f346ca893a020d56ec33b5
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-11-06 15:24:00 +0200

    clean unnecessary actions

--- app/actions/search.rb
+++ app/actions/search.rb
@@ -5,16 +5,6 @@ class Flickrr < Sinatra::Base
   include WillPaginate::Sinatra::Helpers
 
   get '/search' do
-    if params[:search].try(:[], :text)
-      search_query = params[:search][:text]
-      @current_user.histories.create(data: search_query)
-      @photos ||= FlickrService.new.photos(text: search_query)
-        .paginate(page: params[:page], per_page: Config::PAGINATION_PER_PAGE)
-
-      haml :search
-    else
-      redirect to('/gallery')
-    end
+    haml :search
   end
 end
-
--- spec/actions/login_spec.rb
+++ spec/actions/login_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe 'Login actions' do
+  let(:user) do
+    User.create(email: 'test@test.com',
+                password: '12345',
+                password_confirmation: '12345')
+  end
   describe 'GET login' do
     it 'shows the login page' do
       get '/login'
@@ -9,16 +14,21 @@ RSpec.describe 'Login actions' do
     end
 
     it 'logs in the user' do
-      user = User.create(email: 'test@test.com',
-                         password: '12345',
-                         password_confirmation: '12345')
-
       post '/login', { user: { email: user.email, password: '12345' } }
 
       follow_redirect!
 
       expect(last_response).to be_ok
       expect(last_request.url).to eq('http://example.org/search')
+    end
+
+    it 'does not login the user' do
+      post '/login', { user: { email: user.email, password: 'falsey' } }
+
+      follow_redirect!
+
+      expect(last_response).to be_ok
+      expect(last_request.url).to eq('http://example.org/login')
     end
   end
 end
